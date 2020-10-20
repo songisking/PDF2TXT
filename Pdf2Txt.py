df9c4444798c497dc567c0d7295a121128183b12
Song <huangjiawei@bupt.edu.cn>
2020-10-20 23:40:10 +0800

    Update Pdf2Txt.py

--- Pdf2Txt.py
+++ Pdf2Txt.py
@@ -9,13 +9,14 @@ import os
 import re
 import sys
 import io
-# from pdfminer.pdfpage import PDFPage
+from pdfminer.pdfpage import PDFPage
 # from pdfminer.converter import TextConverter
-from pdfminer.pdfparser import PDFParser, PDFDocument
+from pdfminer.pdfparser import PDFParser
+from pdfminer.pdfdocument import PDFDocument
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.layout import LAParams
 from pdfminer.converter import PDFPageAggregator
-from pdfminer.pdfinterp import PDFTextExtractionNotAllowed
+# from pdfminer.pdfinterp import PDFTextExtractionNotAllowed
 
 # 解析文本用到的类：
 # PDFParser（文档分析器）：从文件中获取数据
@@ -35,16 +36,17 @@ def onePdfToTxt(filepath, outpath):
         #创建一个pdf文档分析器
         parser = PDFParser(fp)
         #创建一个PDF文档
-        doc= PDFDocument()
-        #连接分析器 与文档对象
-        parser.set_document(doc)
-        doc.set_parser(parser)
+        doc= PDFDocument(parser)
+        # 连接分析器 与文档对象
+        # parser.set_document(doc)
+        # doc.set_parser(parser)
         # 提供初始化密码doc.initialize("lianxipython")
         # 如果没有密码 就创建一个空的字符串
-        doc.initialize("")
+        # doc.initialize("")
         # 检测文档是否提供txt转换，不提供就忽略
         if not doc.is_extractable:
-            raise PDFTextExtractionNotAllowed
+            #raise PDFTextExtractionNotAllowed
+            pass
 
         else:
             #创建PDf资源管理器
@@ -56,7 +58,7 @@ def onePdfToTxt(filepath, outpath):
             #创建解释器，对文档编码，解释成Python能够识别的格式
             interpreter = PDFPageInterpreter(resource,device)
             # 循环遍历列表，每次处理一页的内容 doc.get_pages() 获取page列表
-            for page in doc.get_pages():
+            for page in enumerate(PDFPage.create_pages(doc)):
                 #利用解释器的process_page()方法解析读取单独页数
                 interpreter.process_page(page)
                 #使用聚合器get_result()方法获取内容
